54680c3f239efe0f5d3736f81e6726767863bc05
Edmund Wong <ewong@envisio.com>
2020-05-07 08:31:54 -0700

    Add format strings for duration

---
+++ src/formatters/duration.js
@@ -0,0 +1,42 @@
+import padStart from '../util/pad_start';
+
+PART_LENGTH = {
+  SECOND: 1000,
+  MINUTE: 60000,
+  HOUR: 3600000,
+  DAY: 86400000,
+}
+
+PART_LIMIT = {
+  SECOND: 60,
+  MINUTE: 60,
+  HOUR: 24,
+}
+
+const formatDurationTotal = (durationType) => (duration) => {
+  const absUnitMillisec = Math.abs(parseInt(duration), 10);
+  const totalDuration = absUnitMillisec / PART_LENGTH[durationType];
+
+  return Math.floor(totalDuration);
+}
+
+export default formatDuration = (durationType, modifier) => (duration) => {
+  const totalDuration = formatDurationTotal(durationType)(duration);
+  const durationPart = modifier === "PART" && PART_LIMIT[durationType] ? totalDuration % PART_LIMIT[durationPart] : totalDuration;
+
+  return padStart(durationPart, 2, 0);
+}
+
+// export default function formatDuration(timestamp) {
+//   var numberDuration = timestamp;
+
+//   var unixSec = parseInt(numberDuration, 10) / 1000;
+//   var negative = unixSec < 0;
+//   var absUnixSec = negative ? -unixSec : unixSec;
+//   var totalMinutes = Math.floor(absUnixSec / 60);
+//   var seconds = absUnixSec % 60;
+//   var hours = Math.floor(totalMinutes / 60);
+//   var minutes = totalMinutes % 60;
+
+//   return (negative ? '-' : '') + padStart(hours, 2, 0) + ":" + padStart(minutes, 2, 0) + ":" + padStart(seconds, 2, 0);
+// };
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import formatDuration from 'formatters/duration';
+
+DURATION_TYPE_KEY = {
+  s: 'SECOND',
+  m: 'MINUTE',
+  h: 'HOUR',
+  d: 'DAY'
+};
+
+DURATION_MODIFIER = {
+  p: 'PART',
+  t: 'TOTAL',
+};
+
+
+export default function HCAdditionalFormatting(H) {
+  const durationFormats = Object.keys(DURATION_TYPE_KEY).reduce((acc, durationKey) => {
+    for (const durationModKey in DURATION_MODIFIER) {
+      acc[`D${durationKey}${durationModKey}`] = formatDuration(
+        DURATION_TYPE_KEY[durationKey],
+        DURATION_MODIFIER[durationModKey],
+      );
+    }
+
+    return acc;
+  }, {});
+
+  H.dateFormats = {
+    DN: (duration) => duration < 0 ? '-' : '',
+    ...durationFormats,
+    ...H.dateFormats,
+  };
+}
